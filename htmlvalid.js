2659e6c872d7c6ed91422f266ae3167175576d6f
manasdigin <manasdavidraj@gmail.com>
2023-04-20 16:44:55 +0530

    project

---
+++ htmlvalid.js
@@ -0,0 +1,117 @@
+const form = document.querySelector('#form')
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const cpassword = document.querySelector('#cpassword');
+const phonenumber=document.querySelector('#phonenumber')
+
+form.addEventListener('submit',(e)=>{
+    
+    if(!validateInputs()){
+        e.preventDefault();
+    }
+})
+
+function validateInputs(){
+    const usernameVal = username.value.trim();
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+    const cpasswordVal = cpassword.value.trim();
+    const phonenumberval=phonenumber.value.trim();
+    let success = true
+
+    if(usernameVal===''){
+        success=false;
+        setError(username,'Username is required')}
+    
+    else{
+        setSuccess(username)
+    }
+
+    if(emailVal===''){
+        success = false;
+        setError(email,'Email is required')
+    }
+    else if(!validateEmail(emailVal)){
+        success = false;
+        setError(email,'Please enter a valid email')
+    }
+    else{
+        setSuccess(email)
+    }
+
+    if(passwordVal === ''){
+        success= false;
+        setError(password,'Password is required')
+    }
+    else if(passwordVal.length<8){
+        success = false;
+        setError(password,'Password must be atleast 8 characters long')
+    }
+    else{
+        setSuccess(password)
+    }
+
+    if(cpasswordVal === ''){
+        success = false;
+        setError(cpassword,'Confirm password is required')
+    }
+    else if(cpasswordVal!==passwordVal){
+        success = false;
+        setError(cpassword,'Password does not match')
+    }
+    else{
+        setSuccess(cpassword)
+    }
+
+    if (phonenumberval === ''){
+        success=false;
+        setError(phonenumber,"number is required")
+    }
+    else if(!validateMob(phonenumberval)){
+        success = false;
+        setError(phonenumber,'Please enter a valid phone number')
+    }
+
+    else if (phonenumber.length<10){
+        success=false;
+        setError(phonenumber,"must be atleast 10 characters")
+    }
+
+    else{
+        setSuccess(phonenumber)
+    }
+    
+
+    return success;
+
+}
+//element - password, msg- pwd is reqd
+function setError(element,message){
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector('.error')
+
+    errorElement.innerText = message;
+
+    
+}
+
+function setSuccess(element){
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector('.error')
+console.log(errorElement)
+   errorElement.innerText = '';
+}
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const validateMob =(phonenumber)=>{
+    return (phonenumber)
+    .match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im);
+  }
